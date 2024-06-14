403ab159134265781d7da047ad781f89b6bed9f2
tushar <tusharkumarshah14394@gmail.com>
2024-06-14 16:26:22 +0530

    tech stack added

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React from "react";
+import Marquee from "react-fast-marquee";
 
 import { companies, testimonials } from "@/data";
 import { InfiniteMovingCards } from "./ui/InfiniteCards";
@@ -25,7 +26,32 @@ const Clients = () => {
           />
         </div>
 
-        <div className="flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-10">
+
+        <Marquee autoFill={true}
+        speed={75}
+        pauseOnHover={true}
+          // remove bg-white dark:bg-black dark:bg-grid-white/[0.05], h-[40rem] to 30rem , md:h-[30rem] are for the responsive design
+          className="rounded-md flex flex-col antialiased  items-center justify-center relative overflow-hidden"
+        >
+          {companies.map((company) => (
+            <React.Fragment key={company.id}>
+              <div className="flex md:max-w-60 max-w-32 gap-2">
+                <img
+                  src={company.img}
+                  alt={company.name}
+                  className="md:w-16 w-5 mx-4"
+                />
+              </div>
+              
+            </React.Fragment>
+          ))}
+          
+        </Marquee>
+
+
+      </div>
+      
+      {/* <div className="flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-10">
           {companies.map((company) => (
             <React.Fragment key={company.id}>
               <div className="flex md:max-w-60 max-w-32 gap-2">
@@ -41,10 +67,12 @@ const Clients = () => {
                   className="md:w-24 w-20"
                 />
               </div>
+              
             </React.Fragment>
           ))}
-        </div>
-      </div>
+        </div> */}
+
+        
     </section>
   );
 };
