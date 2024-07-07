89fe47bdb69246e74782284935054e3986092f0d
tushar <tusharkumarshah14394@gmail.com>
2024-07-07 23:49:01 +0530

    changes

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -9,14 +9,10 @@ import { companies} from "@/data";
 
 const Skills = () => {
   return (<>
-    {/* <section id="" className="py-20">
-      <div className="flex flex-col items-center max-lg:mt-10 "> */}
-        
       {/* marquee section */}
       <Marquee autoFill={true}
         speed={75}
         pauseOnHover={true}
-          // remove bg-white dark:bg-black dark:bg-grid-white/[0.05], h-[40rem] to 30rem , md:h-[30rem] are for the responsive design
           className="rounded-md flex flex-col antialiased  items-center justify-center relative overflow-hidden"
         >
           {companies.map((company) => (
@@ -28,11 +24,9 @@ const Skills = () => {
                   className="md:w-16 w-5 mx-4"
                 />
               </div>
-              
             </React.Fragment>
           ))}
         </Marquee>
-      
     </>
   );
 };
